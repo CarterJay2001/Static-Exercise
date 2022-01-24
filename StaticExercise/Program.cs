8ab8bc7aa5de0bef52b392904db6142b32274761
Jaylon Carter <carterjay2001@outlook.com>
2022-01-24 14:37:13 -0600

    intial commit early version

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to go from:" +
+                "\nFahrenheit to celsius (press 1)" +
+                "\nor Celsius to fahrenheit");
+            int response = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nPick a number!\n");
+
+            if (response == 1)
+            {
+                Console.WriteLine(TempConverter.FahrenheitToCelsius(double.Parse(Console.ReadLine())));
+            }
+            else
+            {
+                Console.WriteLine(TempConverter.CelsiusToFahrenheit(double.Parse(Console.ReadLine())));
+            }
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double num)
+        {
+            int ans = (int)(num - 32);
+            return ans * .5556;
+        }
+
+        public static double CelsiusToFahrenheit (double num)
+        {
+            int ans = (int)(num * 1.8);
+            return ans + 32;
+        }
+    }
+}
